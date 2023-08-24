78aed9d0f758f852bcf3ef2adddd5488aeabefb5
nazarKuznetsov <cuzyalol@gmail.com>
2023-08-24 11:07:14 +0300

    Add empty project

---
+++ swiftUI/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Admin on 24.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
