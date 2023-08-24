cb6c2f073bcc7df68b4826e684da74c8edca8987
nazarKuznetsov <cuzyalol@gmail.com>
2023-08-24 11:43:27 +0300

    Add setting for CircleImage

--- swiftUI/Landmarks/Landmarks/CircleImage.swift
+++ swiftUI/Landmarks/Landmarks/CircleImage.swift
@@ -11,6 +11,10 @@ struct CircleImage: View {
     var body: some View {
         Image("turtlerock")
             .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
         
     }
 }
