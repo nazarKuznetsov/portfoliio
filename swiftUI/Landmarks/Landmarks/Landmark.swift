5521e5bec32e33038392ab06081e0d7c6b4cd1b5
nazarKuznetsov <cuzyalol@gmail.com>
2023-08-24 13:21:09 +0300

    Define a Landmark structure with a few properties matching names of some of the keys in the landmarkData.json data file.

--- swiftUI/Landmarks/Landmarks/Landmark.swift
+++ swiftUI/Landmarks/Landmarks/Landmark.swift
@@ -5,4 +5,25 @@
 //  Created by Admin on 24.08.2023.
 //
 
-import Foundation
+import SwiftUI
+import
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    
+    struct Coordinates: Hashable, Codable {
+            var latitude: Double
+            var longitude: Double
+        }
+}
