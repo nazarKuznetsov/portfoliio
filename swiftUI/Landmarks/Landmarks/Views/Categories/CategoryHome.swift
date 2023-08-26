a243315b4ecf97d0c1dc53de0f9fbaa9b7b67e04
nazarKuznetsov <cuzyalol@gmail.com>
2023-08-26 09:35:24 +0300

    Add a Category View

---
+++ swiftUI/Landmarks/Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Admin on 26.08.2023.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
