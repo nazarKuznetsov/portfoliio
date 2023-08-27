f3d1c42245baad71a05bae5867596adc167a43d8
nazarKuznetsov <cuzyalol@gmail.com>
2023-08-27 13:17:06 +0300

    Add a watchOS Target

---
+++ swiftUI/Landmarks/Landmarks/Views/PageControl.swift
@@ -0,0 +1,20 @@
+//
+//  PageControl.swift
+//  Landmarks
+//
+//  Created by Admin on 27.08.2023.
+//
+
+import SwiftUI
+
+struct PageControl: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PageControl_Previews: PreviewProvider {
+    static var previews: some View {
+        PageControl()
+    }
+}
---
+++ swiftUI/Landmarks/WatchLandmarks Watch App/WatchLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLandmarksApp.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Admin on 27.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct WatchLandmarks_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
