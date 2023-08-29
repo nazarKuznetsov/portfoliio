38859e139cf4a34175f2c59a02bafd7c8cb909f1
nazarKuznetsov <cuzyalol@gmail.com>
2023-08-29 11:16:10 +0300

    Add empty project to repo

---
+++ swiftUI/MyCrypto/MyCrypto/Extensions/Color.swift
@@ -0,0 +1,10 @@
+//
+//  Color.swift
+//  MyCrypto
+//
+//  Created by Admin on 29.08.2023.
+//
+
+import SwiftUI
+
+extension
---
+++ swiftUI/MyCrypto/MyCrypto/MyCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCryptoApp.swift
+//  MyCrypto
+//
+//  Created by Admin on 29.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
