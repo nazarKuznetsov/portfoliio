202b03079fddfa435e1677aa4bcc4c512e89aceb
nazarKuznetsov <cuzyalol@gmail.com>
2023-08-31 18:01:32 +0300

    Implement a custom and reusable search bar

---
+++ swiftUI/MyCrypto/MyCrypto/Core/Home/Components/SearchBarView.swift
@@ -0,0 +1,55 @@
+//
+//  SearchBarView.swift
+//  MyCrypto
+//
+//  Created by Admin on 31.08.2023.
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    
+    @Binding var searchText: String
+    
+    var body: some View {
+        HStack {
+            Image(systemName: "magnifyingglass")
+                .foregroundColor(
+                    searchText.isEmpty ?
+                    Color.theme.secondaryText : Color.theme.accent
+                )
+            
+            TextField("Search by name or symbol", text: $searchText)
+                .foregroundColor(Color.theme.accent)
+                .autocorrectionDisabled()
+                .overlay(
+                    Image(systemName: "xmark.circle.fill")
+                        .padding()
+                        .offset(x:10)
+                        .opacity(searchText.isEmpty ? 0.0 : 1.0)
+                        .onTapGesture {
+                            UIApplication.shared.endEditing()
+                            searchText = ""
+                        }
+                        .foregroundColor(Color.theme.accent),
+                    alignment: .trailing
+                )
+        }
+        .font(.headline)
+        .padding()
+        .background(
+            RoundedRectangle(cornerRadius: 25)
+                .fill(Color.theme.background)
+                .shadow(
+                    color: Color.theme.accent.opacity(0.30),
+                    radius: 10, x: 0, y: 0)
+        )
+        .padding()
+    }
+}
+
+struct SearchBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchBarView(searchText: .constant(""))
+    }
+}
--- swiftUI/MyCrypto/MyCrypto/Core/Home/View/HomeView.swift
+++ swiftUI/MyCrypto/MyCrypto/Core/Home/View/HomeView.swift
@@ -20,6 +20,8 @@ struct HomeView: View {
             VStack {
                 homeHeader
                 
+                SearchBarView(searchText: $homeViewModel.searchText)
+                
                 columnTitles
                 
                 if !showPortfolio {
