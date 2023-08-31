44134379399a3aff1090e1b22a0d91dc600db442
nazarKuznetsov <cuzyalol@gmail.com>
2023-08-31 18:31:51 +0300

    Filtering data based on search bar text

--- swiftUI/MyCrypto/MyCrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ swiftUI/MyCrypto/MyCrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -23,10 +23,26 @@ class HomeViewModel: ObservableObject {
     }
     
     func addSubscribers() {
-        dataService.$allCoins
-            .sink { returnedCoins in
-                self.allCoin = returnedCoins
+        
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filterCoins)
+            .sink { [weak self] (returnedCoins) in
+                self?.allCoin = returnedCoins
             }
             .store(in: &cancellables)
+    }
+    
+    private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
+        guard !text.isEmpty else { return coins }
+        
+        let lowercasedText = text.lowercased()
+        
+        return coins.filter { (coin) -> Bool in
+            return coin.name.lowercased().contains(lowercasedText) ||
+            coin.symbol.lowercased().contains(lowercasedText) ||
+            coin.id.lowercased().contains(lowercasedText)
+        }
     }
 }
