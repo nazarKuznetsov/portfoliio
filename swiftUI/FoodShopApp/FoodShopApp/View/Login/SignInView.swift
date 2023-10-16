31ec33b6080263745adabf109827113f1556676d
nazarKuznetsov <cuzyalol@gmail.com>
2023-10-16 15:18:31 +0300

    Create SignInView

---
+++ swiftUI/FoodShopApp/FoodShopApp/View/Login/SignInView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInView.swift
+//  FoodShopApp
+//
+//  Created by Admin on 16.10.2023.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInView()
+}
