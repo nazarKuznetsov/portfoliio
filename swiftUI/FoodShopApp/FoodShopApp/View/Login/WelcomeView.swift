862a199384976bb3e7cc29ca5b3389b94f2a450e
nazarKuznetsov <cuzyalol@gmail.com>
2023-10-16 15:21:37 +0300

    Create a link to SignInView from WelcomeView

--- swiftUI/FoodShopApp/FoodShopApp/View/Login/WelcomeView.swift
+++ swiftUI/FoodShopApp/FoodShopApp/View/Login/WelcomeView.swift
@@ -34,8 +34,12 @@ struct WelcomeView: View {
                     .multilineTextAlignment(.center)
                     .padding(.bottom, 20)
                 
-                RoundButton(title: "Get Started") {
-                    
+                NavigationLink {
+                    SignInView()
+                } label: {
+                    RoundButton(title: "Get Started") {
+                        
+                    }
                 }
                 
                 Spacer()
@@ -53,5 +57,7 @@ struct WelcomeView: View {
 }
 
 #Preview {
-    WelcomeView()
+    NavigationView{
+        WelcomeView()
+    }
 }
