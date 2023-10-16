b44fea4bcebfe25e67de4ed1b831fb8c0f0be344
nazarKuznetsov <cuzyalol@gmail.com>
2023-10-16 15:13:04 +0300

    Create and setting welcome view

--- swiftUI/FoodShopApp/FoodShopApp/FoodShopAppApp.swift
+++ swiftUI/FoodShopApp/FoodShopApp/FoodShopAppApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct FoodShopAppApp: App {
     var body: some Scene {
         WindowGroup {
-            WelcomeView()
+            NavigationView{
+                WelcomeView()
+            }
         }
     }
 }
--- swiftUI/FoodShopApp/FoodShopApp/View/Login/WelcomeView.swift
+++ swiftUI/FoodShopApp/FoodShopApp/View/Login/WelcomeView.swift
@@ -13,7 +13,39 @@ struct WelcomeView: View {
             Image("welcom_bg")
                 .resizable()
                 .scaledToFill()
+                .frame(width: .screenWidth, height: .screenHeight)
+            VStack {
+                Spacer()
+                
+                Image("app_logo")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 60, height: 60)
+                    .padding(.bottom, 8)
+                
+                Text("Welcome\nto our store")
+                    .font(.customfont(.semibold, fontSize: 48))
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                
+                Text("Get your groceries in as fast as one hour")
+                    .font(.customfont(.medium, fontSize: 16))
+                    .foregroundColor(.white.opacity(0.7))
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom, 20)
+                
+               
+                
+                Spacer()
+                    .frame(height: 20)
+                
+               
+            }
+            .padding(.horizontal, 20)
         }
+        .navigationTitle("")
+        .navigationBarBackButtonHidden(true)
+        .navigationBarHidden(true)
         .ignoresSafeArea()
     }
 }
