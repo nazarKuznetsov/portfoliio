b26c30a2e390d2d1149d1d3e5c6824f2fa5fd365
nazarKuznetsov <cuzyalol@gmail.com>
2023-10-20 12:27:40 +0300

    Create Project

---
+++ swiftUI/SearchYou/SearchYou/SearchYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchYouApp.swift
+//  SearchYou
+//
+//  Created by Admin on 20.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SearchYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
