132e2a45ab349ee6e8bddcb53688c5f530da3d28
nazarKuznetsov <cuzyalol@gmail.com>
2024-04-09 15:15:12 +0300

    Change UIExtension file

--- swiftUI/FoodShop/FoodShop/UICommon/UIExtension.swift
+++ swiftUI/FoodShop/FoodShop/UICommon/UIExtension.swift
@@ -5,39 +5,153 @@
 //  Created by Admin on 09.04.2024.
 //
 
-import Foundation
+import SwiftUI
 
-extension String {
+enum Gilroy: String {
+    case regular = "Gilroy-Regular"
+    case medium = "Gilroy-Medium"
+    case semibold = "Gilroy-SemiBold"
+    case bold = "Gilroy-Bold"
+}
+
+extension Font {
     
-    var isValidEmail: Bool {
-        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
-        let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
-        return emailTest.evaluate(with: self)
+    static func customfont(_ font: Gilroy, fontSize: CGFloat) -> Font {
+        custom(font.rawValue, size: fontSize)
+    }
+}
+
+extension CGFloat {
+    
+    static var screenWidth: Double {
+        return UIScreen.main.bounds.size.width
     }
     
-    func stringDateToDate(format: String = "yyyy-MM-dd'T'HH:mm:ss.SSSZ") -> Date? {
-        let dataFormat = DateFormatter()
-        dataFormat.dateFormat = format
-        return dataFormat.date(from: self)
+    static var screenHeight: Double {
+        return UIScreen.main.bounds.size.height
     }
     
-    func stringDateChangeFormat(format: String, newFormat: String ) -> String {
-        let dataFormat = DateFormatter()
-        dataFormat.dateFormat = format
-        if let dt = dataFormat.date(from: self) {
-            dataFormat.dateFormat = newFormat
-            return dataFormat.string(from: dt)
-        }else{
-            return ""
+    static func widthPer(per: Double) -> Double {
+        return screenWidth * per
+    }
+    
+    static func heightPer(per: Double) -> Double {
+        return screenHeight * per
+    }
+    
+    static var topInsets: Double {
+        if let keyWindow = UIApplication.shared.keyWindow {
+            return keyWindow.safeAreaInsets.top
+        }
+        return 0.0
+    }
+    
+    static var bottomInsets: Double {
+        if let keyWindow = UIApplication.shared.keyWindow {
+            return keyWindow.safeAreaInsets.bottom
+        }
+        return 0.0
+    }
+    
+    static var horizontalInsets: Double {
+        if let keyWindow = UIApplication.shared.keyWindow {
+            return keyWindow.safeAreaInsets.left + keyWindow.safeAreaInsets.right
+        }
+        return 0.0
+    }
+    
+    static var verticalInsets: Double {
+        if let keyWindow = UIApplication.shared.keyWindow {
+            return keyWindow.safeAreaInsets.top + keyWindow.safeAreaInsets.bottom
+        }
+        return 0.0
+    }
+    
+}
+
+extension Color {
+    
+    static var primaryApp: Color {
+        return Color(hex: "53B175")
+    }
+    
+    static var primaryText: Color {
+        return Color(hex: "030303")
+    }
+    
+    static var secondaryText: Color {
+        return Color(hex: "828282")
+    }
+    
+    static var textTitle: Color {
+        return Color(hex: "7C7C7C")
+    }
+    
+    static var placeholder: Color {
+        return Color(hex: "B1B1B1")
+    }
+    
+    static var darkGray: Color {
+        return Color(hex: "4C4F4D")
+    }
+    
+    
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: .alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+            case 3: // RGB(12 -bit)
+                (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+            case 6: // RGB (24-bit)
+                (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+            case 8: // ARGB (32-bit)
+                (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+            default:
+                (a, r, g, b) = (1, 1, 1, 0)
+        }
+        
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue:  Double(b) / 255,
+            opacity: Double(a) / 255
+        )
+    }
+}
+
+struct ShowButton: ViewModifier {
+    @Binding var isShow: Bool
+    
+    public func body(content: Content) -> some View {
+        
+        HStack {
+            content
+            Button {
+                isShow.toggle()
+            } label: {
+                Image(systemName: !isShow ? "eye.fill" : "eye.slash.fill" )
+                    .foregroundColor(.textTitle)
+            }
+
         }
     }
 }
 
-extension Date {
-    func displayDate(format: String, addMinTime:  Int = 0) -> String {
-        let dataFormat = DateFormatter()
-        dataFormat.dateFormat = format
-        let date = self.addingTimeInterval(TimeInterval(60 * addMinTime))
-        return dataFormat.string(from: date)
+extension View {
+    func cornerRadius(_ radius: CGFloat, corner:  UIRectCorner) -> some View {
+        clipShape(RoundedCorner(radius: radius, corers: corner))
+    }
+}
+
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corers: UIRectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corers, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
     }
 }
