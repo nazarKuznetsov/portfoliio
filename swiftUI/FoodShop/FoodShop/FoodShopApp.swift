1838ac340741ec051cd854be438f810ccd5df022
nazarKuznetsov <cuzyalol@gmail.com>
2024-04-09 13:57:54 +0300

    Create new project

---
+++ swiftUI/FoodShop/FoodShop/FoodShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodShopApp.swift
+//  FoodShop
+//
+//  Created by Admin on 09.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct FoodShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
