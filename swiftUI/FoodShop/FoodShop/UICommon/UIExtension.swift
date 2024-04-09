3e73cfb733a9f690871d5d6e5118a4e9fe58b880
nazarKuznetsov <cuzyalol@gmail.com>
2024-04-09 15:12:57 +0300

    Change WelcomeView

---
+++ swiftUI/FoodShop/FoodShop/UICommon/UIExtension.swift
@@ -0,0 +1,8 @@
+//
+//  UIExtension.swift
+//  FoodShop
+//
+//  Created by Admin on 09.04.2024.
+//
+
+import Foundation
--- swiftUI/FoodShop/FoodShop/View/Login/WelcomeView.swift
+++ swiftUI/FoodShop/FoodShop/View/Login/WelcomeView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct WelcomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("welcom_bg")
+                .resizable()
+                .scaledToFill()
+                
+        }
+        .ignoresSafeArea()
     }
 }
 
