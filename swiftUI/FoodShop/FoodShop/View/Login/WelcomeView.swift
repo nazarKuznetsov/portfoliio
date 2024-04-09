d90856d892c5fdfe8ed9c7acbac9ff2957b6e433
nazarKuznetsov <cuzyalol@gmail.com>
2024-04-09 14:57:18 +0300

    Add WelcomeView to project

---
+++ swiftUI/FoodShop/FoodShop/View/Login/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  FoodShop
+//
+//  Created by Admin on 09.04.2024.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
